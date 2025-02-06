bd3ccd551b1597cd8ba761bf10df1fdb8e032f70
kangrae <kangrae0501@gmail.com>
2025-02-06 13:29:47 +0900

    refactor: 불필요한 setter 사용 지양

--- src/main/java/podo/seat/entity/Seat.java
+++ src/main/java/podo/seat/entity/Seat.java
@@ -1,17 +1,21 @@
 package podo.seat.entity;
 
-import podo.seat.constace.SeatStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import podo.seat.constace.SeatStatus;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Seat {
 
     @Id
@@ -26,5 +30,13 @@ public class Seat {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private SeatStatus status;
+
+    public void changeStatus() {
+        if (this.status == SeatStatus.AVAILABLE) {
+            this.status = SeatStatus.RESERVED;
+        } else if (this.status == SeatStatus.RESERVED) {
+            this.status = SeatStatus.AVAILABLE;
+        }
+    }
 
 }
