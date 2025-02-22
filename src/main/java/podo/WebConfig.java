05703c42b8bdd63b49a7e6a97bc65674de6a66a6
gyunho <gyunh01@pukyong.ac.kr>
2025-02-22 18:36:53 +0900

    chore: 패키지 변경 및 오타 수정

--- src/main/java/podo/WebConfig.java
+++ src/main/java/podo/WebConfig.java
@@ -1,3 +1,5 @@
+package podo;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000, https://podoall.netlify.app/")
+                .allowedOrigins("http://localhost:3000", "https://podoall.netlify.app/")
                 .allowedMethods("OPTIONS", "GET", "POST","PUT","DELETE");
     }
 }
