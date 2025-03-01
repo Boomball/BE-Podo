d58121322244ec79d6f7befa24bbad574e188e88
gyunho <gyunh01@pukyong.ac.kr>
2025-03-02 00:58:01 +0900

    fix: 잘못된 엔드포인트 수정

--- src/main/java/podo/WebController.java
+++ src/main/java/podo/WebController.java
@@ -9,7 +9,7 @@ public class WebController implements ErrorController {
 
     // 새로고침 시 dashboard로 매핑되는 컨트롤러가 존재하지 않아 발생하는 404 found를 해결
     @GetMapping({
-            "/api/dashboard"
+            "/dashboard"
     })
     public String redirect() {
         // index.html은 리액트 프로젝트를 빌드 후 /resources/static에 위치시킨다
